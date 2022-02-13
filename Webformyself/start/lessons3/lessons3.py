05589c76569ace915a1c1c06edeb25fc8f39dc28
chenagoold <chenagoold@gmail.com>
2022-02-13 21:48:52 +0300

    13.02.22

---
+++ Webformyself/start/lessons3/lessons3.py
@@ -0,0 +1,55 @@
+##Урок 1
+##Арифметические операторы 
+print(2+3)  #5
+print(10/2) #5
+print(3//2) # 1
+print(7%2)  #1
+print(7//2) #3
+print(-5-3) #-8
+print(5**2) #25
+print(5**3) #125
+print(100**0.5) #10.0
+print(2+3*4) #14
+print((2+3)*4)
+print(0.5+2) #2.5
+print(0.1+0.3)
+
+
+# Урок 2
+#Переменные 
+
+x = 1
+y = 3.5
+my_var1 = 50
+_myvar1 = 100
+myVar = 30
+
+print(type(x))
+print(type(y))
+
+x = 2
+x = 15
+print(x, y)
+
+TEST = 20
+
+# Урок 3
+# Boolen тип данных
+
+
+# is not defined print(text)
+
+# str() int() float()
+
+x = 5.2
+
+print(x, type(x))
+
+x = int(x)
+
+print(x, type(x))
+
+x = 0
+
+x = bool(x)
+print(x, type(x))
